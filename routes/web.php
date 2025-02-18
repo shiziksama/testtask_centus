7e132bce9de40bf6e13995e5584771ec732949b2
Andrei Karepin <karepinandrei@gmail.com>
2025-02-18 11:41:48 +0200

    added jetstream

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', [SubscriptionController::class, 'index']);
 Route::post('/', [SubscriptionController::class, 'subscribe'])->name('subscribe');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
